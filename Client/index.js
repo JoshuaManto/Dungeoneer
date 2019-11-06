b4b9ed9fb6c4d6ac4521908f72e22961ec8cfc50
JoshuaManto <joshuamanto@yahoo.com>
2019-11-05 23:48:39 -0800

    Added browser router

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+// render(<App />, document.getElementById('root'));
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
