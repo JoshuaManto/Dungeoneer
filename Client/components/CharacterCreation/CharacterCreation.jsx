55540698b308b7c7376d4beea636f13acb6b1631
JoshuaManto <joshuamanto@yahoo.com>
2019-11-06 19:35:56 -0800

    Character Creation base template

---
+++ Client/components/CharacterCreation/CharacterCreation.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const CharacterCreation = props => {
+  return (
+    <div className="characterCreation">
+      <h1>character creation</h1>
+
+      {/* <form>
+        <label>Name</label>
+        <input
+          type="text"
+          value={props.characterName}
+          onChange={props.characterNameChangeHandler}
+        ></input>
+        <label>Background</label>
+        <input
+          type="text"
+          value={props.characterBackground}
+          onChange={props.characterBackgroundChangeHandler}
+        ></input>
+      </form> */}
+    </div>
+  );
+};
+
+export default CharacterCreation;
