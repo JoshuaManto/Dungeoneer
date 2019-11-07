89a493ba9f499d33ea804b4bfe77e524cc66798a
JoshuaManto <joshuamanto@yahoo.com>
2019-11-07 01:19:08 -0800

    Refactored db query to accomodate promise calls

--- Server/models/Model.js
+++ Server/models/Model.js
@@ -27,12 +27,14 @@ const pool = new Pool({
 module.exports = {
   query: (text, params, callback) => {
     const start = Date.now();
-    return pool.query(text, params, (err, res) => {
-      const duration = Date.now() - start;
-      // console.log(res);
-      // console.log('Executed query ', { text, duration, rows: res.rowCount });
-      callback(err, res);
-    });
+    if (callback)
+      return pool.query(text, params, (err, res) => {
+        const duration = Date.now() - start;
+        // console.log(res);
+        // console.log('Executed query ', { text, duration, rows: res.rowCount });
+        callback(err, res);
+      });
+    return pool.query(text, params);
   }
 };
 
