e636fae3fd6a622d39542750c777369b32580181
JoshuaManto <joshuamanto@yahoo.com>
2019-11-06 19:39:12 -0800

    Routes to all and single class(es)

---
+++ Server/routes/classesRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const classesController = require('../controllers/classesController');
+
+const router = express.Router();
+
+router.get(
+  '/:class_name',
+  classesController.getSingleClass,
+  (req, res) => console.log('in router get all classes')
+  // res.status(200).json([...res.locals.characters])
+);
+
+router.get(
+  '/',
+  classesController.getAllClasses,
+  (req, res) => console.log('in router get all classes')
+  // res.status(200).json([...res.locals.characters])
+);
+
+module.exports = router;
