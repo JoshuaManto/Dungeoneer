9ef2bcf21f5d65e174248e3736518a639374fb2d
JoshuaManto <joshuamanto@yahoo.com>
2019-11-07 01:13:00 -0800

    Added buttons to route to classes, races, and alignments

--- Client/components/CharacterCreation/CharacterCreation.jsx
+++ Client/components/CharacterCreation/CharacterCreation.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const CharacterCreation = props => {
   return (
     <div className="characterCreation">
       <h1>character creation</h1>
+
+      <button onClick={props.classHandler}>Class</button>
+
+      <button onClick={props.raceHandler}>Race</button>
+
+      <button onClick={props.alignmentHandler}>Alignment</button>
+
+      {/* <Link to="/classes">Class</Link> */}
 
       {/* <form>
         <label>Name</label>
