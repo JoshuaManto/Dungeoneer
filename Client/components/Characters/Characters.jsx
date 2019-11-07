aaecd59b13a88fcff787ebdd46335839699d8bdc
JoshuaManto <joshuamanto@yahoo.com>
2019-11-07 01:14:24 -0800

    Renders a list of characters

--- Client/components/Characters/Characters.jsx
+++ Client/components/Characters/Characters.jsx
@@ -5,9 +5,19 @@ import Character from './Character/Character';
 import CharacterCreation from '../CharacterCreation/CharacterCreation';
 
 const Characters = props => {
+  const charactersArr = props.characters.map(character => (
+    <Character
+      characterDetails={character}
+      classList={props.classList}
+      raceList={props.raceList}
+    ></Character>
+  ));
+
   return (
     <div>
       <h1>List of characters</h1>
+
+      {charactersArr}
 
       <Link to="characters/creation">create character</Link>
     </div>
