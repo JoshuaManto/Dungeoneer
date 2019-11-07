fe3ede022a9144dc38530367fe2214358df50cb7
JoshuaManto <joshuamanto@yahoo.com>
2019-11-07 01:13:44 -0800

    Renders a single character

--- Client/components/Characters/Character/Character.jsx
+++ Client/components/Characters/Character/Character.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Character = props => {
+  console.log(props.characterDetails);
   return (
     <div>
       <h1>character</h1>
+      {/* <div>{props.characterDetails && props.characterDetails.Name}</div> */}
+      <div>{props.characterDetails.Name}</div>
+      <div>{props.characterDetails.Level}</div>
+      <div>{props.classList[props.characterDetails.class_id]}</div>
+      <div>{props.raceList[props.characterDetails.race_id]}</div>
     </div>
   );
 };
